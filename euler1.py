3e957ed5337d4c90051531148f96119584e620f0
mgric <xplode7@gmail.com>
2014-11-19 21:17:41 -0500

    add first project euler solution

---
+++ euler1.py
@@ -0,0 +1,20 @@
+import math
+
+
+n= 0
+m = 0
+l=0
+
+for i in range (0,1000,3):
+	n = n +i
+
+for j  in range (0,1000,5):	
+	m = m +j
+
+for k in range (0,1000,15):
+	l = l+k
+
+z = m +n-l
+
+print(z)
+
